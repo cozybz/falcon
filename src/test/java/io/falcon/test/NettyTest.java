cf7e4d07fdb8121348afc8c95b2063b989506de2
cozybz <cozybz@gmail.com>
2015-04-15 23:44:58 +0800

    初始化目录

--- src/test/java/io/falcon/test/NettyTest.java
+++ src/test/java/io/falcon/test/NettyTest.java
@@ -1,8 +1,65 @@
 package io.falcon.test;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.util.ReferenceCountUtil;
+import org.junit.Test;
+
 /**
  * Created by cozybz@gmail.com on 2015/4/15.
  */
 public class NettyTest {
 
+    @Test
+    public void runServer() {
+        EventLoopGroup masterGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        ServerBootstrap bootstrap = new ServerBootstrap();
+        bootstrap.group(masterGroup, workerGroup).channel(NioServerSocketChannel.class);
+        bootstrap.childHandler(new ChannelInitializer<SocketChannel>() {
+            @Override
+            protected void initChannel(SocketChannel socketChannel) throws Exception {
+                socketChannel.pipeline().addLast(new TestHandler());
+            }
+        });
+        bootstrap.option(ChannelOption.SO_BACKLOG, 128);
+
+        try {
+            ChannelFuture future = bootstrap.bind(8080).sync();
+
+            future.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            workerGroup.shutdownGracefully();
+            masterGroup.shutdownGracefully();
+        }
+    }
+
+    class TestHandler extends ChannelInboundHandlerAdapter {
+        @Override
+        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+            ByteBuf in = (ByteBuf) msg;
+            try {
+                while (in.isReadable()) {
+                    System.out.print((char) in.readByte());
+                    System.out.flush();
+                }
+            } finally {
+                ReferenceCountUtil.release(msg);
+            }
+        }
+
+        @Override
+        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+            cause.printStackTrace();
+            ctx.close();
+        }
+    }
+
 }
+
