274f17428c7ff51f0078407273b2ec804b6e07d0
cozybz <cozybz@gmail.com>
2015-04-15 23:22:43 +0800

    初始化目录

---
+++ src/main/java/io/falcon/bootstrap/FalconFactory.java
@@ -0,0 +1,7 @@
+package io.falcon.bootstrap;
+
+/**
+ * Created by cozybz@gmail.com on 2015/4/15.
+ */
+public class FalconFactory {
+}
---
+++ src/test/java/io/falcon/test/NettyTest.java
@@ -0,0 +1,8 @@
+package io.falcon.test;
+
+/**
+ * Created by cozybz@gmail.com on 2015/4/15.
+ */
+public class NettyTest {
+
+}
