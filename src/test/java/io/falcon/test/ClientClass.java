a768504ac44c0f1b824f9a9f50bc6839a3991b5a
cozybz <cozybz@126.com>
2015-04-16 10:14:49 +0800

    增加Client测试类

---
+++ src/test/java/io/falcon/test/ClientClass.java
@@ -0,0 +1,13 @@
+package io.falcon.test;
+
+import org.junit.Test;
+
+/**
+ * Created by cozybz@gmail.com on 2015/4/16.
+ */
+public class ClientClass {
+    @Test
+    public void runClient() {
+
+    }
+}
